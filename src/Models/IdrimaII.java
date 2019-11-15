5c93a77342e29ac60cfd5eddff7d5151cf564a75
Michael Angelo Georgiou <michailangelos.georgiou@gmail.com>
2019-11-15 17:26:08 +0200

    Update IdrimaII

--- src/Models/IdrimaII.java
+++ src/Models/IdrimaII.java
@@ -18,6 +18,52 @@ public class IdrimaII {
 
     private static IdrimaII idrimaInstance = null;
 
+    public IdrimaII(){}
+
+    public IdrimaII(String onoma, String dieuthinsi, String istoselida, int tilefono){
+    this.onoma = onoma;
+    this.dieuthinsi= dieuthinsi;
+    this.istoselida = istoselida;
+    this.tilefono = tilefono;
+    }
+
+    public String getOnoma(){ return onoma;}
+
+    public void setOnoma(String onoma) {
+        this.onoma = onoma;
+    }
+
+    public int getTilefono() {
+        return tilefono;
+    }
+
+    public void setTilefono(int tilefono) {
+        this.tilefono = tilefono;
+    }
+
+    public String getDieuthinsi() {
+        return dieuthinsi;
+    }
+
+    public void setDieuthinsi(String dieuthinsi) {
+        this.dieuthinsi = dieuthinsi;
+    }
 
 
+    public String getIstoselida() {
+        return istoselida;
+    }
+
+    public void setIstoselida(String istoselida) {
+        this.istoselida = istoselida;
+    }
+    public static IdrimaII getIdrimaInstance() {
+        return idrimaInstance;
+    }
+
+
+
+    public static void setUniversityInstance(IdrimaII idrimaInstance) {
+        IdrimaII.idrimaInstance = idrimaInstance;
+    }
 }
